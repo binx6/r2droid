d55d77ffbf197059671349c8775a58ef37771a96
DESKTOP-PP7V2VN\Wsdx233 <18082792459@163.com>
2026-03-14 23:42:30 +0800

    修改版本号准备发布v0.2.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "top.wsdx233.r2droid"
         minSdk = 24
         targetSdk = 26
-        versionCode = 2603120
-        versionName = "0.2.10"
+        versionCode = 2603140
+        versionName = "0.2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
